6f681b4da774679a26e5b04c236151ce104ecfa9
Ritik-455 <rvjaat25@gmail.com>
2023-11-22 12:39:01 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+function openNav() {
+    document.getElementById("navbar").classList.toggle("start-0");
+    document.body.classList.toggle("overflow_hidden");
+    document.querySelector(".menu").classList.toggle("cross")
+  }
+  
+  const Preloader = document.getElementById('preloader')
+  setTimeout(() => {
+      console.log("Delayed for 1 second.");
+
+      Preloader.classList.add("preloader-finish")
+  }, 2000);
+  
