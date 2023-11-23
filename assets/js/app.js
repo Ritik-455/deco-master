db6d27d14ffea291941c81b6df2baa1a0c488177
Ritik-455 <rvjaat25@gmail.com>
2023-11-23 09:17:57 +0530

    changes in pre-loader

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ function openNav() {
   const Preloader = document.getElementById('preloader')
   setTimeout(() => {
       console.log("Delayed for 1 second.");
-
+      document.body.classList.remove("overflow-hidden");
       Preloader.classList.add("preloader-finish")
   }, 2000);
   
